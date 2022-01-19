2e57acb8499b2cc65b5d2596dfc962491c9919bc
MohonaJS <mohona.chowdhury@iths.se>
2022-01-19 19:35:01 +0100

    todo app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
-    <div>
-      <form action="" @submit="handleformsubmit">
-        <div><label for="">Your Name: </label>
-          <input type="text"></div>
-        <button type="submit">Submit form </button>
-      </form>
-    </div>
+   <form action="" @submit.prevent="addTask">
+     <h1>To-Do app</h1>
+     <p>Find all the task in one place</p>
+     <input type="text" placeholder="Add a task" v-model="gettodo"> 
+     <button > Submit </button>
+   </form>
+   <ol>
+     <li v-for="(todo, index) in todos" :key="index">
+        {{todo.text}}
+     </li>
+   </ol>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {
-  }, 
-  data(){return{  
-    name: ''
+  data(){ 
+    return { 
+      gettodo: '',
+      todos: [
+        {  text: 'Buy eggs', done:true},
+        {  text: 'Buy oil', done:false},
+        {  text: 'Buy alu', done:false}
+      ]
   }},
-  methods:{  
-    handleformsubmit(e){  
-      e.preventDefault();
-      
+  methods: {  
+    addTask(){  
+     console.log(this.todos);
     }
   }
 }
 </script>
 
 <style>
-
+  button, input {
+    margin: 5px;
+    padding: 5px;
+   
+    color: black;
+  }
 </style>
