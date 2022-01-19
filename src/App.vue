adca891ee2550f57f79fb10da0bb4739beb898bd
MohonaJS <mohona.chowdhury@iths.se>
2022-01-19 08:04:15 +0100

    form v-model

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div>
+      <form action="" @submit="handleformsubmit">
+        <div><label for="">Your Name: </label>
+          <input type="text"></div>
+        <button type="submit">Submit form </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }, 
+  data(){return{  
+    name: ''
+  }},
+  methods:{  
+    handleformsubmit(e){  
+      e.preventDefault();
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
